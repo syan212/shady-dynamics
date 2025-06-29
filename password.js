5aa65e7dcf9ffbbb78957ad6d327a47220fcb1de
syan212 <syan235711@gmail.com>
2025-06-29 12:10:39 +0100

    Create password.js

---
+++ password.js
@@ -0,0 +1,27 @@
+async function submitPassword() {
+  try{
+    console.log('Start');
+    const entered_password = prompt('Enter');
+    if (!entered_password) return;
+    const response = await fetch('/.netlify/functions/compare-hash', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({password: entered_password}),
+    });
+    const {match} = await response.json();
+    console.log(match);
+    if (match){
+      alert('Correct Password!');
+      sessionStorage.setItem('authenticated', 'true');
+      window.location.href = 'index.html';
+    }
+    else{
+      alert('Wrong Password');
+    }
+  } catch (error){
+    console.error(error);
+    alert('Error')
+      }
+}
