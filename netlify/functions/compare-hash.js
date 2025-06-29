1513ee123ef1369e5d95afba16b04376d48e3374
syan212 <syan235711@gmail.com>
2025-06-29 11:14:35 +0100

    Create compare-hash.js

---
+++ netlify/functions/compare-hash.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt');
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: 'Method Not Allowed',
+    };
+  }
+
+  try {
+    const storedHash = '$2b$10$zDsb9ZaO4PScY3xLJnfzMeiWGCAZkT0rYpi4eikj1ceqI7yqKqixu';
+    const { password } = JSON.parse(event.body || '{}');
+
+    const match = await bcrypt.compare(password, storedHash);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ match }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
