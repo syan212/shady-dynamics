fe5a1b15f46884d500e4f8999108099ea7fc9c8e
syan212 <syan235711@gmail.com>
2025-07-01 19:24:29 +0100

    Fix Indents

--- password/password.js
+++ password/password.js
@@ -1,26 +1,31 @@
 async function submitPassword() {
-  try{
-    const entered_password = document.getElementById('password_field').value;
-    if (!entered_password) return;
-    const response = await fetch('/.netlify/functions/compare-hash', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({password: entered_password}),
-    });
-    const {match} = await response.json();
-    console.log(match);
-    if (match){
-      alert('Correct Password!');
-      sessionStorage.setItem('authenticated', 'true');
-      window.location.href = '/index.html';
+	const password_output = document.querySelector('#screen #login-container #password-output');
+  	try{
+    	const entered_password = document.getElementById('password_field').value;
+    	if (!entered_password) return;
+    	const response = await fetch('/.netlify/functions/compare-hash', {
+      		method: 'POST',
+      		headers: {
+        		'Content-Type': 'application/json',
+      		},
+      		body: JSON.stringify({password: entered_password}),
+    	});
+    	const {match} = await response.json();
+    	console.log(match);
+    	if (match){
+			password_output.style = 'color: black;'
+      		password_output.innerHTML = 'Correct Password. You have been authenticated.';
+      		sessionStorage.setItem('authenticated', 'true');
+      		window.location.href = '/index.html';
+    	}
+    	else{
+			password_output.style = 'color: red;'
+      		password_output.innerHTML = 'Wrong Password. Please try again.';
+			await new Promise(r => setTimeout(r, 750));
+			password_output.style = 'color: black;'
+    	}
+    }catch (error){
+    	console.error(error);
+    	alert('Error')
     }
-    else{
-      alert('Wrong Password');
-    }
-  } catch (error){
-    console.error(error);
-    alert('Error')
-      }
 }
