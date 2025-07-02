b93fe1e575c2846756131bd97ebd114dc301d101
syan212 <syan235711@gmail.com>
2025-07-02 20:29:17 +0100

    Password Output Fix

--- password/password.js
+++ password/password.js
@@ -1,6 +1,5 @@
 async function submitPassword() {
 	const password_output = document.querySelector('#screen #login-container #password-output');
-	password_output.style = 'visibility: visible;';
   	try{
     	const entered_password = document.getElementById('password_field').value;
     	if (!entered_password) return;
@@ -15,13 +14,14 @@ async function submitPassword() {
     	console.log(match);
     	if (match){
       		password_output.innerHTML = 'Correct Password. You have been authenticated.';
+			password_output.style = 'visibility: visible;';
 			await new Promise(r => setTimeout(r, 200));
       		sessionStorage.setItem('authenticated', 'true');
       		window.location.href = '/index.html';
     	}
     	else{
-			password_output.style = 'color: red;';
       		password_output.innerHTML = 'Wrong Password. Please try again.';
+			password_output.style = 'color: red; visibility: visible;';
 			await new Promise(r => setTimeout(r, 750));
 			password_output.style = 'color: black;'
     	}
