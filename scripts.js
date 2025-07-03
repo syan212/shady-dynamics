cd81217977b125ebc4f85a8791ba12d93ccc7167
syan212 <syan235711@gmail.com>
2025-07-03 20:20:10 +0100

    Move Script to script.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,32 @@
-let lock = true
-let version = "v1.0.0"
+if (sessionStorage.getItem('authenticated') !== 'true') {
+    window.location.href = 'password/password.html';
+}
+		
+window.addEventListener("DOMContentLoaded", function(){
+	const test_page = document.getElementById('test-page');
+  	test_page.click();
+});
 
-function buttonLink(link) {
-    location.replace(link)
+
+function buttontest() {
+    alert("I don't think I need to say this but this is a button test. If you see this, the button works as expected.");
 }
 
-function showAdminPrompt(){
-    if (sessionStorage.getItem('isAdmin') == 'true') {
-        let adminPrompt = prompt("$System-Administrator: ");
-        if (adminPrompt == "testCommand") {
-            alert("testCommand Successful")
-        }
-    }
-}
-
-function alertMessage(message){
-    alert(message);
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";  
+	}
+	tablinks = document.getElementsByClassName("tab");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	try{
+		document.getElementById(tabName).style.display = "block";  
+	}
+	catch (error){
+		console.error("This tab's content doesn't exist");
+	}
+	evt.currentTarget.className += " active";
 }
